c0925e3e157f0e4c61b46b637817e5d7d4f2f55c
Mohamed Ashraf <maahmed@mc.gov.sa>
2024-08-09 16:35:48 +0300

    Add project files.

---
+++ Infrastructure/InfrasturctureDI.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrasturctureDI
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("BankingControlConnection");
+
+        return services;
+    }
+}
